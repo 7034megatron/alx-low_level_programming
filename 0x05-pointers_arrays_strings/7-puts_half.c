8a2c3ddbefc0b6b58a6829d84a97aae4f74a817b
7034megatron <emmanuemabhena0@gmail.com>
2023-07-25 12:41:59 -0700

    qsn 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	char *tmp = str;
+       	int len = _strlen(tmp);
+	int start_index;
+
+    if (length % 2 == 0) {
+        start_index = length / 2;
+    } else {
+        start_index = (length + 1) / 2;
+    }
+	for (int i = start_index; i < length; i++) 
+	{
+        	_putchar(str[i]);
+   	 }
+    _putchar('\n');      
+}
