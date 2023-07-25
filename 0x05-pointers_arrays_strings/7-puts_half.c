4dfeb724e60b9812d2b2621889bd8121e197fa81
7034megatron <emmanuemabhena0@gmail.com>
2023-07-25 12:47:43 -0700

    qsn 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 void puts_half(char *str)
-{
+{	
+	int i;
+	int start_index;
 	char *tmp = str;
        	int length = _strlen(tmp);
-	int start_index;
 
     if (length % 2 == 0) {
         start_index = length / 2;
     } else {
         start_index = (length + 1) / 2;
     }
-    	int i;
+  
 	for (i = start_index; i < length; i++) 
 	{
         	_putchar(str[i]);
