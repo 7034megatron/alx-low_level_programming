828dcb2e53cdb2474648b282af830d75b007f979
7034megatron <emmanuemabhena0@gmail.com>
2023-07-25 10:48:58 -0700

    hello

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int count = 0;
+	while(str* != '\0')
+	{
+		str++;
+		count++;
+	}
+	while(count != 0)
+	{
+		str--;
+		count--;
+		_puchar(count);
+	}
+	_putchar(count);
+}
