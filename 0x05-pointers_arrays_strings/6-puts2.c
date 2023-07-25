45aed3bdf34f5aa751b9716b888ee883b244ff0d
7034megatron <emmanuemabhena0@gmail.com>
2023-07-25 11:52:50 -0700

    qns6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+void puts2(char *str) 
+{
+	while (*str != '\0')
+        {
+                _putchar(*str);
+                str++;
+		str++;
+        }
+        _putchar('\n');
+}
