b74037e870dd07b7d70ae0454e3653049b6e7e89
7034megatron <emmanuemabhena0@gmail.com>
2023-07-25 12:07:31 -0700

    qns6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,7 +5,7 @@ void puts2(char *str)
 {
 	int index = 0;
     while (str[index] != '\0' && str[index + 1] != '\0') {
-     	_putchar(str[index);
+     	_putchar(str[index]);
         index += 2;
     }
     _putchar('\n');
