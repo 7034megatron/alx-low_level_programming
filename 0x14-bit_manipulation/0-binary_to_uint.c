ff674ecef53c103743e62348b91cad88fcdc6218
7034megatron <emmanuelmabhena0@gmail.com>
2023-09-28 10:33:54 -0700

    second commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,30 @@
 unsigned int binary_to_uint(const char *b)
 {
     unsigned int result = 0;
+    unsigned int bit_value = 1;
 
     if (b == NULL)
-        return (0);
+        return 0;
 
-    while (*b)
+    // Calculate the length of the input string without using the standard library
+    unsigned int length = 0;
+    while (b[length] != '\0')
+        length++;
+
+    // Check for invalid characters in the string
+    for (unsigned int i = 0; i < length; i++)
     {
-        if (*b != '0' && *b != '1')
-            return (0);
-
-        result = (result * 2) + (*b - '0');
-        b++;
+        if (b[i] != '0' && b[i] != '1')
+            return 0; // Invalid character found
     }
 
-    return (result);
+    // Convert binary string to unsigned int
+    for (int i = length - 1; i >= 0; i--)
+    {
+        if (b[i] == '1')
+            result += bit_value;
+        bit_value <<= 1;
+    }
+
+    return result;
 }
-
-
-
-
-
