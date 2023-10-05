984ea291fdf1c194afc20d5ab7e2322ab27b2d3e
7034megatron <emmanuelmabhena0@gmail.com>
2023-10-04 23:30:05 -0700

    hello

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,42 +11,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd, t, w;
+	char *buf;
+	ssize_t fd, t, w;
 
-    /* Open the file for reading */
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	/* Open the file for reading */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	/* Allocate memory for the buffer */
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    /* Allocate memory for the buffer */
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	/* Read data from the file into the buffer */
+	t = read(fd, buf, letters);
+	if (t == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-    /* Read data from the file into the buffer */
-    t = read(fd, buf, letters);
-    if (t == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-
-    /* Write the data from the buffer to STDOUT */
-    w = write(STDOUT_FILENO, buf, t);
-    if (w == -1 || w != t)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-
-    /* Clean up and return the actual number of bytes read and printed */
-    free(buf);
-    close(fd);
-    return (w);
+	/* Write the data from the buffer to STDOUT */
+	w = write(STDOUT_FILENO, buf, t);
+	if (w == -1 || w != t)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	/* Clean up and return the actual number of bytes read and printed */
+	free(buf);
+	close(fd);
+	return (w);
 }
